07dbf23008cd303ade04656c1c20bb1b577de7e2
MarkStranger1 <art225.03@gmail.com>
2024-04-27 15:33:37 +0400

    Add model Dish

--- backend/dish/models.py
+++ backend/dish/models.py
@@ -3,6 +3,7 @@ from django.conf import settings
 
 
 class Ingredient(models.Model):
+    """Модель ингредиентов"""
     name = models.CharField(
         verbose_name="Название",
         max_length=settings.LIMIT_CHAR_200
@@ -40,7 +41,43 @@ class Type(models.Model):
     class Meta:
         verbose_name = 'Тип'
         verbose_name_plural = 'Типы'
-        default_related_name = 'types'
+        default_related_name = 'Types'
 
     def __str__(self):
         return f'{self.name}'
+
+
+class Dish(models.Model):
+    name = models.CharField(
+        verbose_name="Название",
+        max_length=settings.LIMIT_CHAR_100
+    )
+    description = models.CharField(
+        verbose_name="Описание",
+        max_length=settings.LIMIT_CHAR_500
+    )
+    cost = models.IntegerField(
+        verbose_name="Стоимость",
+        min_value=settings.MIN_INT_0,
+        step_size=settings.STEP_INT_50
+    )
+    ccal = models.IntegerField(
+        verbose_name="Килокалории",
+        min_value=settings.MIN_INT_0
+    )
+    weight = models.IntegerField(
+        verbose_name="Вес",
+        min_value=settings.MIN_INT_0
+    )
+    image = models.ImageField(
+        verbose_name="Фотография",
+        upload_to="dishes/"
+    )
+
+    class Meta:
+        verbose_name = "Блюдо"
+        verbose_name_plural = "Блюда"
+        default_related_name = "Dishes"
+
+    def __str__(self):
+        return f'{self.name}{self.description}{self.cost}{self.ccal}{self.weight}{self.image}'
