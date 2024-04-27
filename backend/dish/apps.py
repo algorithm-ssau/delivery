8cad3ad52a3de5aa63045feebab8333716c505c8
MarkStranger1 <art225.03@gmail.com>
2024-04-27 13:32:23 +0400

    фÐadd class Teg and Ingredient

--- backend/dish/apps.py
+++ backend/dish/apps.py
@@ -4,5 +4,3 @@ from django.apps import AppConfig
 class DishConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dish'
-
-#Я надеюсь, что это последняя проверка аккаунта
