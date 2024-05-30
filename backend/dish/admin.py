25cc9aec9edcba8dfba3f7d5f555a2cde41efd01
MarkStranger1 <art225.03@gmail.com>
2024-05-30 14:47:20 +0400

    change in dish/admin.py

--- backend/dish/admin.py
+++ backend/dish/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
 
-from .models import (Ingredient)
+from .models import (Ingredient, Type)
 
 admin.site.unregister(Group)
 
@@ -14,4 +14,15 @@ class IngredientAdmin(admin.ModelAdmin):
     list_filter = ('name',)
     search_fields = ('name',)
     list_editable = ('name', 'measurement_unit',)
+    ordering = ('-name',)
+
+
+@admin.register(Type)
+class TagAdmin(admin.ModelAdmin):
+    """Настройка Type для панели Admin"""
+
+    list_display = ('pk', 'name', 'slug')
+    list_filter = ('name',)
+    search_fields = ('name',)
+    list_editable = ('name', 'slug')
     ordering = ('-name',)
