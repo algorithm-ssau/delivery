f6d56a0a5e31a556d3f815fd42bfbe372957edf9
MarkStranger1 <art225.03@gmail.com>
2024-05-30 13:57:53 +0400

    add types.csv 2.0

--- backend/dish/admin.py
+++ backend/dish/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from .models import (Ingredient)
+
+admin.site.unregister(Group)
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    """Настройка Ingredient для панели Admin"""
+
+    list_display = ('pk', 'name', 'measurement_unit')
+    list_filter = ('name',)
+    search_fields = ('name',)
+    list_editable = ('name', 'measurement_unit',)
